054f82bab3014fcc7779826ab511544211807ef0
Izuho <72969994+Izuho@users.noreply.github.com>
2022-10-24 17:26:50 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -4,13 +4,15 @@ from circuit.ansatz import Frax_ansatz, replace_Frax_ansatz
 import torch.distributed as dist
 import math
 
+def lastbit_Z(state):
+    return torch.sum(state[0:len(state):2].abs()**2)-torch.sum(state[1:len(state):2].abs()**2)
+
 class FraxClassify():
     def __init__(self, n_qubits, layer_size, measure_iter, world_size):
         self.n_qubits = n_qubits
         self.layer_size = layer_size
         self.measure_iter = measure_iter
         self.params = (torch.zeros(layer_size, n_qubits, 3) + 1/math.sqrt(3)).to(torch.complex64)
-        self.R = torch.zeros(3, 3)
         self.world_size =world_size
         self.train_acc = []
         self.test_acc = []
@@ -21,7 +23,9 @@ class FraxClassify():
         x = amplitude_embedding(train_feat, self.n_qubits)
         for a in range(self.layer_size):
             for b in range(self.n_qubits):
+                R = torch.zeros(3,3)
                 for c in range(train_feat.shape[0]):
+                    
                     for d in range(a):
                         x[c] = Frax_ansatz(self.n_qubits, params[d]) @ x[c]
                     rx = replace_Frax_ansatz(self.n_qubits, b, 'X', params[a]) @ x[c]
@@ -38,27 +42,28 @@ class FraxClassify():
                         rxz = Frax_ansatz(self.n_qubits, params[d]) @ rxz        
                         ryz = Frax_ansatz(self.n_qubits, params[d]) @ ryz
                         
-                    rx = torch.sum(rx[0:len(rx):2].abs()**2)-torch.sum(rx[1:len(rx):2].abs()**2)
-                    ry = torch.sum(ry[0:len(ry):2].abs()**2)-torch.sum(ry[1:len(ry):2].abs()**2)
-                    rz = torch.sum(rz[0:len(rz):2].abs()**2)-torch.sum(rz[1:len(rz):2].abs()**2)
-                    rxy = torch.sum(rxy[0:len(rxy):2].abs()**2)-torch.sum(rxy[1:len(rxy):2].abs()**2)
-                    rxz = torch.sum(rxz[0:len(rxz):2].abs()**2)-torch.sum(rxz[1:len(rxz):2].abs()**2)
-                    ryz = torch.sum(ryz[0:len(ryz):2].abs()**2)-torch.sum(ryz[1:len(ryz):2].abs()**2)
+                    rx = lastbit_Z(rx)
+                    ry = lastbit_Z(ry)
+                    rz = lastbit_Z(rz)
+                    rxy = lastbit_Z(rxy)
+                    rxz = lastbit_Z(rxz)
+                    ryz = lastbit_Z(ryz)
                         
-                    self.R[0,0] += train_label[c] * 2 * rx
-                    self.R[0,1] += train_label[c] * (2 * rxy-rx-ry)
-                    self.R[0,2] += train_label[c] * (2 * rxz-rx-rz)
-                    self.R[1,1] += train_label[c] * 2 * ry
-                    self.R[1,2] += train_label[c] * (2 * ryz-ry-rz)
-                    self.R[2,1] += train_label[c] * 2 * rz
+                    R[0,0] += train_label[c] * 2 * rx
+                    R[0,1] += train_label[c] * (2 * rxy-rx-ry)
+                    R[0,2] += train_label[c] * (2 * rxz-rx-rz)
+                    R[1,1] += train_label[c] * 2 * ry
+                    R[1,2] += train_label[c] * (2 * ryz-ry-rz)
+                    R[2,1] += train_label[c] * 2 * rz
                     
-                self.R[1,0] = self.R[0,1]
-                self.R[2,0] = self.R[0,2]
-                self.R[2,1] = self.R[1,2]
+                R[1,0] = R[0,1]
+                R[2,0] = R[0,2]
+                R[2,1] = R[1,2]
                 group = dist.new_group(range(self.world_size))
-                dist.all_reduce(self.R, op=dist.ReduceOp.SUM, group=group)
-                eigenvalues, eigenvectors = torch.linalg.eig(self.R)
-                params[a, b] = eigenvectors[torch.argmin(eigenvalues.real)]
+                dist.all_reduce(R, op=dist.ReduceOp.SUM, group=group)
+                eigenvalues, eigenvectors = torch.linalg.eig(R)
+                self.params[a, b] = eigenvectors[torch.argmin(eigenvalues.real)]
+                self.params[a, b] /= torch.norm(self.params[a, b])
                 
     def eval(self, train, test):
         test_score = 0
@@ -69,18 +74,18 @@ class FraxClassify():
         test_size = test_label.shape[0]
         for a in range(test_size):
             x = amplitude_embedding(test_feat[a], self.n_qubits)
-            for a in range(self.layer_size):
-                x = Frax_ansatz(self.n_qubits, self.params[a]) @ x
-            test_score += test_label[a] * (torch.sum(x[0:len(x):2].abs()**2)-torch.sum(x[1:len(x):2].abs()**2))
+            for b in range(self.layer_size):
+                x = Frax_ansatz(self.n_qubits, self.params[b]) @ x
+            test_score += test_label[a] * lastbit_Z(x)
         group = dist.new_group(range(self.world_size))
         dist.all_reduce(test_score, op=dist.ReduceOp.SUM, group=group)
         self.test_acc.append(test_score)
         
         for a in range(train_size):
             x = amplitude_embedding(train_feat[a], self.n_qubits)
-            for a in range(self.layer_size):
-                x = Frax_ansatz(self.n_qubits, self.params[a]) @ x
-            train_score += train_label[a] * (torch.sum(x[0:len(x):2].abs()**2)-torch.sum(x[1:len(x):2].abs()**2))
+            for b in range(self.layer_size):
+                x = Frax_ansatz(self.n_qubits, self.params[b]) @ x
+            train_score += train_label[a] * lastbit_Z(x)
         group = dist.new_group(range(self.world_size))
         dist.all_reduce(train_score, op=dist.ReduceOp.SUM, group=group)
         self.train_acc.append(train_score)
