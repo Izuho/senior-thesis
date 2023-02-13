f3c7750e0b2c700eee69f6c1a7b9a86d47d9a891
Izuho <72969994+Izuho@users.noreply.github.com>
2023-02-13 20:59:06 +0900

    Add files via upload

--- training.py
+++ training.py
@@ -1,7 +1,6 @@
 import numpy as np
 from model import FraxClassify
 import time
-from qiskit_ibm_runtime import Session
 
 def data_loader(num_train, num_test):
     try:
@@ -33,12 +32,13 @@ def cut_data(train_label, train_feat, test_label, test_feat, rank, world_size):
     
     return train_label[start1:end1], train_feat[start1:end1], test_label[start2:end2], test_feat[start2:end2]
 
-def parallel_train(n_qubits, layer_size, world_size, num_train, num_test, update_iter, service, backend):
+def parallel_train(rank, n_qubits, layer_size, world_size, num_train, num_test, update_iter):
+    print('I am ', rank)
     test_label, train_label, test_feat, train_feat = data_loader(num_train, num_test)
-    model = FraxClassify(n_qubits, layer_size, world_size, num_train, num_test, backend)
-    with Session(service = service, backend = backend):
-        for i in range(update_iter):
-            st = time.time()
-            model.fit_and_eval(train_feat,train_label,test_feat,test_label)
-            print(time.time()-st)
-            print('_______________NEW________________',i)
+    train_label, train_feat, test_label, test_feat = cut_data(train_label, train_feat, test_label, test_feat, rank, world_size)
+    model = FraxClassify(n_qubits, layer_size, world_size, num_train, num_test)
+    for i in range(update_iter):
+        st = time.time()
+        model.fit_and_eval(train_feat,train_label,test_feat,test_label)
+        if rank == 0: print(time.time()-st)
+        if rank == 0: print('_______________NEW________________',i)
